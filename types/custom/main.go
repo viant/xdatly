617d7e34dc648c6ab1d29a5ba04c457464aa4e7a
klarysz <klarysz@viantinc.com>
2023-02-22 00:54:21 +0100

    removed main method

--- types/custom/main.go
+++ types/custom/main.go
@@ -3,7 +3,3 @@ package main
 import (
 	_ "github.com/viant/xdatly/types/custom/imports"
 )
-
-//in order to use it as library, main package has to have function main
-//do not delete.
-func main() {}
