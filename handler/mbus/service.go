1718014518f1a2c8b81090197adf60a172cbb197
awitas <adrianwit@gmail.com>
2023-06-08 19:39:32 -0700

    updated message options

--- handler/mbus/service.go
+++ handler/mbus/service.go
@@ -1,6 +1,8 @@
 package mbus
 
+import "context"
+
 type Service interface {
-	Push(message *Message) (*Confirmation, error)
+	Push(ctx context.Context, message *Message) (*Confirmation, error)
 	Message(dest string, data interface{}, opts ...Option) *Message
 }
