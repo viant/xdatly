f29cae06483b72fb2723fcd8acb4f6f5e181ca59
klarysz <klarysz@viantinc.com>
2023-08-18 02:39:25 +0200

    added PredicateHandler

--- codec/predicate.go
+++ codec/predicate.go
@@ -1,9 +1,15 @@
 package codec
 
+import "context"
+
 type (
 	Predicate struct {
 		Parameter string //name of parameter
 		Template  string //predicate template
 		Criteria
+	}
+
+	PredicateHandler interface {
+		Compute(ctx context.Context, value interface{}) (*Criteria, error)
 	}
 )
