d4fe36f95a8d4e48f5c8581b6e6e1585cf05dc5e
awitas <adrianwit@gmail.com>
2023-08-22 20:29:32 -0700

    updated codec options

--- codec/options.go
+++ codec/options.go
@@ -10,7 +10,9 @@ type Options struct {
 	LookupType  func(name string) (reflect.Type, error)
 	Record      interface{}
 	LookupValue func(ctx context.Context, name string) (interface{}, error)
-	Options     []interface{}
+	ColumnsSource
+	Selector
+	Options []interface{}
 }
 
 func (o *Options) Apply(opts []Option) {
@@ -33,6 +35,20 @@ type Option func(o *Options)
 func WithTypeLookup(fn func(name string) (reflect.Type, error)) Option {
 	return func(o *Options) {
 		o.LookupType = fn
+	}
+}
+
+// WithColumnsSource creates columnSource option
+func WithColumnsSource(columnSource ColumnsSource) Option {
+	return func(o *Options) {
+		o.ColumnsSource = columnSource
+	}
+}
+
+// WithSelector creates selector option
+func WithSelector(selector Selector) Option {
+	return func(o *Options) {
+		o.Selector = selector
 	}
 }
 
