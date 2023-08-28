91acb0b441e583c6a3739343674015d788f0cbae
awitas <adrianwit@gmail.com>
2023-08-28 14:33:44 -0700

    added NormalizedName

--- predicate/filter.go
+++ predicate/filter.go
@@ -1,16 +1,16 @@
 package predicate
 
 type StringsFilter struct {
-	Include []string
-	Exclude []string
+	Include []string `json:",omitempty"`
+	Exclude []string `json:",omitempty"`
 }
 
 type IntFilter struct {
-	Include []int
-	Exclude []int
+	Include []int `json:",omitempty"`
+	Exclude []int `json:",omitempty"`
 }
 
 type BoolFilter struct {
-	Include []bool
-	Exclude []bool
+	Include []bool `json:",omitempty"`
+	Exclude []bool `json:",omitempty"`
 }
