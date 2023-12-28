d3889b908202a2add530a96ce8c92805797d4b77
adranwit <adrianwit@gmail.com>
2023-12-28 09:53:57 -0800

    added factory

--- handler/handler.go
+++ handler/handler.go
@@ -5,3 +5,7 @@ import "context"
 type Handler interface {
 	Exec(ctx context.Context, session Session) (interface{}, error)
 }
+
+type Factory interface {
+	New(ctx context.Context, arguments ...string) (Handler, error)
+}
