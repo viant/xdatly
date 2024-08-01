dfedf6180efb14870b6e0e8243b32393129416b7
adranwit <adrianwit@gmail.com>
2024-07-31 20:49:20 -0700

    added CanUseMarkerProvider validator option

--- handler/http/error.go
+++
@@ -1,8 +0,0 @@
-package http
-
-import "github.com/viant/xdatly/handler/response"
-
-// NewError creates a new error
-func NewError(code int, message string, object interface{}) *response.Error {
-	return response.NewError(code, message, response.WithObject(object))
-}
--- handler/state/option.go
+++ handler/state/option.go
@@ -1,11 +1,15 @@
 package state
 
+import "github.com/viant/xdatly/handler/http"
+
 type (
 	//Option represents state option
 	Option func(o *Options)
 	//Options represents state options
 	Options struct {
-		scope string
+		scope     string
+		form      *http.Form
+		constants map[string]string
 	}
 )
 
@@ -20,6 +24,33 @@ func WithScope(scope string) Option {
 // Scope returns scope
 func (s *Options) Scope() string {
 	return s.scope
+}
+
+// Form returns form
+func (s *Options) Form() *http.Form {
+	return s.form
+}
+
+// Constants returns constants
+func (s *Options) Constants() map[string]string {
+	return s.constants
+}
+
+// WithConstants returns option with constants
+func WithConstants(key string, value string) Option {
+	return func(o *Options) {
+		if o.constants == nil {
+			o.constants = make(map[string]string)
+		}
+		o.constants[key] = value
+	}
+}
+
+// WithForm returns option with form
+func WithForm(form *http.Form) Option {
+	return func(o *Options) {
+		o.form = form
+	}
 }
 
 // NewOptions returns option with scope
