44594ed681ffda1e1db7da5fdaef9e329b290939
adranwit <adrianwit@gmail.com>
2024-08-01 07:23:13 -0700

    added CanUseMarkerProvider validator option

--- handler/differ/log.go
+++ handler/differ/log.go
@@ -6,3 +6,20 @@ type (
 		Changes []*Change
 	}
 )
+
+// ToChangeRecords converts changeLog to change records
+func (l *ChangeLog) ToChangeRecords(source, id, userID string) []*ChangeRecord {
+	var result []*ChangeRecord
+	for _, change := range l.Changes {
+		result = append(result, &ChangeRecord{
+			Source:   source,
+			SourceID: id,
+			UserID:   userID,
+			Path:     change.Path.String(),
+			Change:   string(change.Type),
+			From:     change.From,
+			To:       change.To,
+		})
+	}
+	return result
+}
