11a0855c8bb504568c08aeb9488445d4f8aea543
adranwit <adrianwit@gmail.com>
2024-09-23 11:52:04 -0700

    added path parameter option

--- handler/state/db.go
+++ handler/state/db.go
@@ -2,5 +2,9 @@ package state
 
 import "database/sql"
 
+type dBProviderKey string
+
+var DBProviderKey dBProviderKey = "dbProvider"
+
 // DBProvider represents a database provider
 type DBProvider func(name string) (*sql.DB, error)
