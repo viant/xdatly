d757eac8abe0fd881cb5bc41256a407eb63e5cc4
adranwit <adrianwit@gmail.com>
2025-01-06 13:54:44 -0800

    added data sync type

--- handler/sync.go
+++ handler/sync.go
@@ -3,3 +3,12 @@ package handler
 import "sync"
 
 type DataSync map[string]*sync.RWMutex
+
+func (d DataSync) Wait(key string) {
+	lock, ok := d[key]
+	if !ok {
+		return
+	}
+	lock.RLock()
+	lock.RUnlock()
+}
