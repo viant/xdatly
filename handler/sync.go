54964d4af51df062dd04cf3a2ff8f289b9205e9c
adranwit <adrianwit@gmail.com>
2025-01-10 11:47:15 -0800

    added data sync type

--- handler/sync.go
+++ handler/sync.go
@@ -2,10 +2,42 @@ package handler
 
 import "sync"
 
-type DataSync map[string]*sync.RWMutex
+// DataSync represents data sync
+type DataSync struct {
+	aMap map[string]*sync.RWMutex
+	rw   sync.RWMutex
+}
 
-func (d DataSync) Wait(key string) {
-	lock, ok := d[key]
+func (d *DataSync) Put(key string) {
+	d.rw.Lock()
+	defer d.rw.Unlock()
+	if _, ok := d.aMap[key]; !ok {
+		d.aMap[key] = &sync.RWMutex{}
+	}
+}
+
+func (d *DataSync) Get(key string) *sync.RWMutex {
+	d.rw.Lock()
+	defer d.rw.Unlock()
+	if _, ok := d.aMap[key]; !ok {
+		return nil
+	}
+	return d.aMap[key]
+}
+
+func (d *DataSync) Delete(key string) {
+	d.rw.Lock()
+	defer d.rw.Unlock()
+	if _, ok := d.aMap[key]; ok {
+		delete(d.aMap, key)
+
+	}
+}
+
+func (d *DataSync) Wait(key string) {
+	d.rw.RLock()
+	lock, ok := d.aMap[key]
+	d.rw.RUnlock()
 	if !ok {
 		return
 	}
